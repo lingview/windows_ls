1ae50b84bf45adf303c339632a4845ac6804876f
澪 <106723621+lingview@users.noreply.github.com>
2024-12-16 20:19:15 +0800

    Update ls.c

--- windows_ls/ls.c
+++ windows_ls/ls.c
@@ -1,15 +1,21 @@
-/**
- * by lingview 
- * https://github.com/lingview/windows_ls
- */
-
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-typedef struct {
+#define COLOR_RESET 0x07
+#define COLOR_DIR 0x09
+#define COLOR_FILE 0x0F
+#define COLOR_EXECUTABLE 0x0E
+#define COLOR_BATCH 0x0C
+
+#define VERSION _T("1.3")
+#define AUTHOR _T("lingview")
+#define GITHUB_URL _T("https://github.com/lingview/windows_ls")
+
+typedef struct
+{
     WIN32_FIND_DATA data;
 } FileEntry;
 
@@ -21,19 +27,45 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
 
 void PrintLongFormat(const WIN32_FIND_DATA *findFileData);
 
+void PrintFileNameWithColor(const WIN32_FIND_DATA *findFileData);
+
 int _tmain(int argc, TCHAR *argv[])
 {
     int i = 1;
-    int listLong = 0; 
-    int showAll = 0;  
+    int listLong = 0;
+    int showAll = 0;
     int recursive = 0;
     int horizontal = 1;
     int sortBySize = 0;
     int sortByTime = 0;
     int showDirsOnly = 0;
 
-    while (i < argc && argv[i][0] == _T('-'))
+    while (i < argc && (argv[i][0] == _T('-') || argv[i][0] == _T('/')))
     {
+        if (_tcscmp(argv[i], _T("--version")) == 0)
+        {
+            _tprintf(_T("版本: %s\n"), VERSION);
+            _tprintf(_T("作者: %s\n"), AUTHOR);
+            _tprintf(_T("源码: %s\n"), GITHUB_URL);
+            return 0;
+        }
+        else if (_tcscmp(argv[i], _T("--help")) == 0)
+        {
+            _tprintf(_T("用法:\n"));
+            _tprintf(_T("  ls [选项] [路径]\n"));
+            _tprintf(_T("选项:\n"));
+            _tprintf(_T("  -l\t长格式列出文件信息，包括权限、大小、修改时间\n"));
+            _tprintf(_T("  -a\t显示所有文件，包括隐藏文件 . 和 ..\n"));
+            _tprintf(_T("  -R\t递归列出指定目录及其子目录中的文件\n"));
+            _tprintf(_T("  -x\t横向列出文件，一行一个文件\n"));
+            _tprintf(_T("  -s\t按文件大小排序\n"));
+            _tprintf(_T("  -d\t仅显示目录\n"));
+            _tprintf(_T("  -t\t按修改时间排序\n"));
+            _tprintf(_T("  --version\t显示版本、作者、源码地址\n"));
+            _tprintf(_T("  --help\t显示此帮助信息\n"));
+            return 0;
+        }
+
         for (LPCTSTR p = &argv[i][1]; *p != _T('\0'); ++p)
         {
             switch (*p)
@@ -56,7 +88,7 @@ int _tmain(int argc, TCHAR *argv[])
             case _T('d'):
                 showDirsOnly = 1;
                 break;
-            case _T('t'): 
+            case _T('t'):
                 sortByTime = 1;
                 break;
             default:
@@ -67,7 +99,6 @@ int _tmain(int argc, TCHAR *argv[])
         ++i;
     }
 
-    // 检查排序选项冲突
     if (sortBySize && sortByTime)
     {
         _ftprintf(stderr, _T("不能同时使用 -s 和 -t 选项。\n"));
@@ -90,13 +121,12 @@ int _tmain(int argc, TCHAR *argv[])
 
     return 0;
 }
-
 void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursive, int horizontal, int sortBySize, int sortByTime, int showDirsOnly)
 {
     WIN32_FIND_DATA findFileData;
     HANDLE hFind = INVALID_HANDLE_VALUE;
     TCHAR szDir[MAX_PATH];
-    _stprintf_s(szDir, MAX_PATH, _T("%s\\*"), lpPath); 
+    _stprintf_s(szDir, MAX_PATH, _T("%s\\*"), lpPath);
 
     hFind = FindFirstFile(szDir, &findFileData);
 
@@ -114,7 +144,7 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
         {
             if (!showAll)
             {
-                // 跳过 . 和 ..
+
                 if (_tcscmp(findFileData.cFileName, _T(".")) == 0 ||
                     _tcscmp(findFileData.cFileName, _T("..")) == 0)
                 {
@@ -122,7 +152,6 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
                 }
             }
 
-            // 仅显示目录
             if (showDirsOnly && !(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
             {
                 continue;
@@ -133,13 +162,11 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
             fileCount++;
         } while (FindNextFile(hFind, &findFileData) != 0);
 
-        // 检查FindNextFile错误
         if (GetLastError() != ERROR_NO_MORE_FILES)
         {
             _tprintf(_T("FindNextFile 失败 (%d)\n"), GetLastError());
         }
 
-        // 排序文件
         if (sortBySize)
         {
             qsort(files, fileCount, sizeof(FileEntry), CompareBySize);
@@ -150,7 +177,7 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
         }
 
         int printedCount = 0;
-        // 打印文件信息
+
         for (int j = 0; j < fileCount; ++j)
         {
             if (listLong)
@@ -161,19 +188,21 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
             {
                 if (horizontal)
                 {
-                    _tprintf(_T("%s\t"), files[j].data.cFileName);
+                    PrintFileNameWithColor(&files[j].data);
+                    _tprintf(_T("\t"));
                     printedCount++;
-                    if (printedCount % 5 == 0) { 
+                    if (printedCount % 5 == 0)
+                    {
                         _tprintf(_T("\n"));
                     }
                 }
                 else
                 {
-                    _tprintf(_T("%s\n"), files[j].data.cFileName);
+                    PrintFileNameWithColor(&files[j].data);
+                    _tprintf(_T("\n"));
                 }
             }
 
-            // 递归列出子目录
             if (recursive && (files[j].data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) &&
                 _tcscmp(files[j].data.cFileName, _T(".")) != 0 &&
                 _tcscmp(files[j].data.cFileName, _T("..")) != 0)
@@ -185,8 +214,8 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
             }
         }
 
-        // 处理横向排布的换行
-        if (horizontal && printedCount % 5 != 0) { 
+        if (horizontal && printedCount % 5 != 0)
+        {
             _tprintf(_T("\n"));
         }
 
@@ -197,8 +226,8 @@ void ListFilesInDirectory(LPCTSTR lpPath, int listLong, int showAll, int recursi
 
 void PrintLongFormat(const WIN32_FIND_DATA *findFileData)
 {
-    TCHAR attrs[6]; 
-    // 格式化文件属性
+    TCHAR attrs[6];
+
     _stprintf_s(attrs, sizeof(attrs) / sizeof(TCHAR), _T("%c%c%c%c%c"),
                 (findFileData->dwFileAttributes & FILE_ATTRIBUTE_READONLY) ? 'r' : '-',
                 (findFileData->dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) ? 'h' : '-',
@@ -212,12 +241,56 @@ void PrintLongFormat(const WIN32_FIND_DATA *findFileData)
 
     SYSTEMTIME st;
     FileTimeToSystemTime(&(findFileData->ftLastWriteTime), &st);
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (findFileData->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_DIR);
+    }
+    else if (_tcsstr(findFileData->cFileName, _T(".exe")))
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_EXECUTABLE);
+    }
+    else
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_FILE);
+    }
+
     _tprintf(_T("%s %I64u %02d/%02d/%04d %02d:%02d %s\n"),
             attrs,
             fileSize.QuadPart,
             st.wMonth, st.wDay, st.wYear,
             st.wHour, st.wMinute,
             findFileData->cFileName);
+
+    SetConsoleTextAttribute(hConsole, COLOR_RESET);
+}
+
+
+void PrintFileNameWithColor(const WIN32_FIND_DATA *findFileData)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (findFileData->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_DIR);
+    }
+    else if (_tcsstr(findFileData->cFileName, _T(".exe")))
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_EXECUTABLE);
+    }
+    else if (_tcsstr(findFileData->cFileName, _T(".bat")))
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_BATCH);
+    }
+    else
+    {
+        SetConsoleTextAttribute(hConsole, COLOR_FILE);
+    }
+
+    _tprintf(_T("%s"), findFileData->cFileName);
+
+    SetConsoleTextAttribute(hConsole, COLOR_RESET);
 }
 
 int CompareBySize(const void *a, const void *b)
@@ -227,7 +300,6 @@ int CompareBySize(const void *a, const void *b)
     sizeA.HighPart = ((FileEntry *)a)->data.nFileSizeHigh;
     sizeB.LowPart = ((FileEntry *)b)->data.nFileSizeLow;
     sizeB.HighPart = ((FileEntry *)b)->data.nFileSizeHigh;
-
 
     if (sizeA.QuadPart < sizeB.QuadPart)
         return -1;
@@ -257,4 +329,6 @@ int CompareByTime(const void *a, const void *b)
  * -s	按文件大小排序
  * -d	仅显示目录
  * -t	按最后修改时间排序
+ * --version 显示版本号、作者信息和开源地址
+ * --help	显示帮助信息
  */
